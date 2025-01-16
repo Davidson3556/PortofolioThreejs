6fb1295366a5b4daccf953e1cc99bcc152568b0f
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2025-01-16 18:21:06 +0100

    production

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from "./sections/Navbar.jsx";
+
+const App = () => {
+    return (
+        <main>
+            <Navbar/>
+        </main>
+    )
+}
+export default App
