1b5bb73fad7a8b89d6ec506e50d8f1e4696e19d8
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2025-01-17 09:39:48 +0100

    hackerroom

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,8 @@
 import { PerspectiveCamera } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
-// import { HackerRoom } from '../components/HackerRoom.jsx';
-
-
+import { HackerRoom } from '../components/HackerRoom.jsx'
+import CanvasLoader from '../components/CanvasLoader.jsx'
 const Hero = () => {
   return (
     <section className="min-h-screen w-full flex flex-col relative">
@@ -15,9 +14,15 @@ const Hero = () => {
 
         <div className="w-full h-full absolute inset-0">
             <Canvas className="w-full h-full">
-                <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-                {/* <HackerRoom /> */}
+                <Suspense fallback={<CanvasLoader/>}>
 
+               
+                <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+                <HackerRoom scale={sizes.deskScale} position={sizes.deskPosition} rotation={[0.1, -Math.PI, 0]} />   
+                
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+                                 </Suspense>
             </Canvas>
 
         </div>
