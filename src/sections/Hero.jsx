92cb894e53c0755fd5a73bae05082a85194efc9a
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2025-01-20 17:11:50 +0100

    hackerrom

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,34 +1,50 @@
-import { PerspectiveCamera } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import React from 'react'
-import { HackerRoom } from '../components/HackerRoom.jsx'
-import CanvasLoader from '../components/CanvasLoader.jsx'
+// import { PerspectiveCamera } from "@react-three/drei";
+// import { Canvas } from "@react-three/fiber";
+import React from "react";
+import { HackerRoom } from "../components/HackerRoom.jsx";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import { PerspectiveCamera } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+
+// import { Leva, useControls } from "leva";
+import { Suspense } from "react";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from '../constants/index.js';
 const Hero = () => {
+    const isSmall = useMediaQuery({ maxWidth: 440 });
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+    const sizes = calculateSizes(isSmall, isMobile, isTablet);    
   return (
     <section className="min-h-screen w-full flex flex-col relative">
-        <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-            <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">Hello, I am Olawale <span className="waving-hand">👋</span></p>
-            <p className="hero_tag text-gray_gradient text-center">Building products & Brands</p>
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+          Hello, I am Olawale <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient text-center">
+          Building products & Brands
+        </p>
+      </div>
 
-        </div>
+      <div className="w-full h-full absolute inset-0">
+        {/* <Leva/> */}
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+            <HackerRoom
+              scale={sizes.deskScale}
+              position={sizes.deskPosition}
+              rotation={[0.1, -Math.PI, 0]}
 
-        <div className="w-full h-full absolute inset-0">
-            <Canvas className="w-full h-full">
-                <Suspense fallback={<CanvasLoader/>}>
-
-               
-                <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-                <HackerRoom scale={sizes.deskScale} position={sizes.deskPosition} rotation={[0.1, -Math.PI, 0]} />   
-                
+            />
+            
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
-                                 </Suspense>
-            </Canvas>
-
-        </div>
-      
+          </Suspense>
+        </Canvas>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
